9bdacec079453f11b68b50c8cf3686111955239b
Alastair Rayls <42526387+AlastairRayls@users.noreply.github.com>
2018-08-20 20:47:39 -0600

    Create Main.java

---
+++ src/github/alastair-rayls/BrainFuck-Compiler/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  
+  public static void main(String[] args) {
+  
+  Gui g = new Gui();
+  
+  g.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+  g.setSize(1080, 760);
+  g.setVisible(true);
+  
+  }
+  
+}
