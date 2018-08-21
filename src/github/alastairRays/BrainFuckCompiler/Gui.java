1cf0cd0221427d5e461700c1a19af394eb1fb2f5
Alastair Rayls <42526387+AlastairRayls@users.noreply.github.com>
2018-08-20 20:58:52 -0600

    Create Gui.java

---
+++ src/github/alastairRays/BrainFuckCompiler/Gui.java
@@ -0,0 +1,17 @@
+package github.alastairRayls.BrainFuckCompiler;
+
+import javax.awt.*;
+import javax.swing.*;
+
+public class Gui extends JFrame {
+
+  public Gui() {
+  
+    super("Brain Fuck Compiler");
+    setLayout(new FlowLayout());
+    
+    
+  
+  }
+
+}
